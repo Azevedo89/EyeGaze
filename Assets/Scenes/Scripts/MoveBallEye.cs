91783d1c9babd7885a456c3654a37eaa24f11a4c
Azevedo89 <goncalosazevedo@gmail.com>
2022-07-19 16:51:05 +0100

    Movimento da Bola com os olhos
    Já meti a bola a movimentar-se completamente com os olhos

--- Assets/Scenes/Scripts/MoveBallEye.cs
+++ Assets/Scenes/Scripts/MoveBallEye.cs
@@ -9,39 +9,62 @@ namespace Tobii.XR.Examples.GettingStarted
     public class MoveBallEye : MonoBehaviour, IGazeFocusable
     {
         public Renderer SphereRend;
+        public Transform target;
+        public float t;
+        public float speed; 
+        public bool Eye;
+        public GameObject respawn; 
 
         public void GazeFocusChanged(bool hasFocus)
         {
+
             //If this object received focus, fade the object's color to highlight color
             if (hasFocus)
             {
                 //Call SetColor using the shader property name "_Color" and setting the color to red
-                SphereRend.material.SetColor("_Color", Color.red);
+                //SphereRend.material.SetColor("_Color", Color.red);
+                Eye = true;
             }
             //If this object lost focus, fade the object's color to it's original color
             else
             {
-                SphereRend.material.SetColor("_Color", Color.green);
+                //SphereRend.material.SetColor("_Color", Color.green);
+                Eye = false;
             }
+        }
+
+        void Awake()
+        {
+            respawn = GameObject.FindGameObjectWithTag("Sphere");
+
         }
 
         // Start is called before the first frame update
         void Start()
         {
             SphereRend = gameObject.GetComponent<Renderer>();
+            target = respawn.transform;
             GazeFocusChanged(false);
+
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            if (Eye)
+            {
+                MoveBallWithEyes();
+            }
         }
 
 
         public void MoveBallWithEyes()
         {
+            //Lerp
+            Vector3 a = transform.position;
+            Vector3 b = target.position;
+            target.position = Vector3.MoveTowards(b, Vector3.Lerp(a, b, t), speed);
 
-        }
+        } 
     }
 }
